746844a681d400a9656a3e6f166a0d6ca477dbba
lu <1622489569@qq.com>
2020-08-14 00:12:20 +0800

    本项目是学习Spring框架的项目

--- src/com/company/User.java
+++ src/com/company/User.java
@@ -5,8 +5,16 @@ package com.company;
  * @create 2020-08-12 17:17
  */
 public class User {
-    public void test(){
-        System.out.println("创建了一个user类来实现这个事情！");
+    private String name;
+    private String age;
+
+    public User(String name, String age) {
+        this.name = name;
+        this.age = age;
     }
 
+    public void test(){
+        System.out.println("创建了一个user类来实现这个事情！");
+        System.out.println(name+""+age);
+    }
 }
